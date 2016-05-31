73025bc92290a55b5cd6577c87b73d4626c58fcc
Maria Announcia <maria@Marias-MacBook-Air.local>
2016-05-31 05:45:13 +0530

    slight modifications to include necessary dependencies

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -21,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname,'/bower_components')));
 app.use('/', routes);
 app.use('/users', users);
 
@@ -31,7 +30,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
 // error handlers
 
 // development error handler
